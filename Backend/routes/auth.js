fa6fdf83ae6a72c117080a1f4ab3deb15f195fd6
chinthanah <chinthanahssuresh@gmail.com>
2023-09-24 11:22:43 +0530

    created login endpoint

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -56,4 +56,45 @@ router.post(
   }
 );
 
+//Authenticate a user using "api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "enter a valid email").isEmail(),
+    body("password", "enter a valid credentials").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const passwordcompare = await bcrypt.compare(password, user.password);
+      if (!passwordcompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
 module.exports = router;
