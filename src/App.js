d5bae740cb6dd751523ca38a4b7bff4855542e5d
chinthanah <chinthanahssuresh@gmail.com>
2023-09-28 22:59:48 +0530

    added context

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import NotesState from "./context/notes/NotesState";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NotesState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NotesState>
     </>
   );
 }
