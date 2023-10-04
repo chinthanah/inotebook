eee28ff5c0debd96efbd8994487585868c80956c
chinthanah <chinthanahssuresh@gmail.com>
2023-10-04 18:25:52 +0530

    added alert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -10,18 +10,36 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   return (
     <>
       <NotesState>
         <Router>
           <Navbar />
-          <Alert message="This is a best react course" />
+          <Alert alert={alert} />
           <div className="container">
             <Routes>
-              <Route exact path="/" element={<Home />} />
+              <Route exact path="/" element={<Home showAlert={showAlert} />} />
               <Route exact path="/about" element={<About />} />
-              <Route exact path="/login" element={<Login />} />
-              <Route exact path="/signup" element={<Signup />} />
+              <Route
+                exact
+                path="/login"
+                element={<Login showAlert={showAlert} />}
+              />
+              <Route
+                exact
+                path="/signup"
+                element={<Signup showAlert={showAlert} />}
+              />
             </Routes>
           </div>
         </Router>
